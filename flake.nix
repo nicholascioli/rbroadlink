3d1f08e923e9e5248f7e53bfc6f2bc252e1d0bd6
Nicholas Cioli <nicholascioli@users.noreply.github.com>
2023-09-16 00:15:09 -0400

    actually add flake support

--- flake.nix
+++ flake.nix
@@ -10,11 +10,48 @@
       let
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage ./.;
+        version = "0.3.0";
+
+        buildExample = example: naersk-lib.buildPackage {
+          inherit version;
+          pname = example;
+          src = ./.;
+
+          # Enable the necessary features for the cli
+          cargoBuildOptions = old: old ++ [
+            "--features ${example}"
+          ];
+
+          # Only build the example
+          overrideMain = old: {
+            preConfigure = ''
+              cargo_build_options="$cargo_build_options --example ${example}"
+            '';
+          };
+        };
+      in {
+        packages = rec {
+          default = rbroadlink-cli;
+          rbroadlink-cli = buildExample "rbroadlink-cli";
+          mqtt-broadlink = buildExample "mqtt-broadlink";
+
+          container = pkgs.dockerTools.buildLayeredImage {
+            name = "ghcr.io/nicholascioli/rbroadlink";
+            tag = "latest";
+            contents = [rbroadlink-cli mqtt-broadlink];
+            config = {
+              Entrypoint = [
+                "${rbroadlink-cli}/bin/rbroadlink-cli"
+              ];
+              Env = [
+                "RUST_LOG=info"
+              ];
+            };
+          };
+        };
+
         devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy act ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
       });
